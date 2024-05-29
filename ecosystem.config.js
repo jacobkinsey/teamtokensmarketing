8058e1ee030dde18b0de4d27d7aed32d35492f82
Jacob Kinsey <jacob.kinsey@gmail.com>
2024-05-29 10:47:33 -0500

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm start",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "TeamTokens-KeyPair.pem",
+      user: "ubuntu",
+      host: "34.200.228.118",
+      ref: "origin/main",
+      repo: "https://github.com/jacobkinsey/teamtokensmarketing.git",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
+};
